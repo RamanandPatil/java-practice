68504bcb8333907ccc60ab25f3a9bc36ef06ca98
RamanandPatil <ramanand.patil@in.ibm.com>
2020-01-09 12:40:00 +0530

    Solved Generic Pair problem from Stepik course Java Classes. This was the last problem.

--- src/GenericPairs.java
+++ src/GenericPairs.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Generic pairs
  *
@@ -23,10 +25,57 @@
  * Do not change the access modifier of the class Pair.
  */
 public class GenericPairs {
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+        System.out.println("i = " + i + ", s = " + s);
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        System.out.println("mustBeTrue: " + mustBeTrue);
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+        System.out.println("mustAlsoBeTrue: " + mustAlsoBeTrue);
+    }
 }
 
 
-class Pair {
+class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
 
+    private Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
+        return new Pair<T1, T2>(first, second);
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+               Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
-
